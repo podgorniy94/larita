fbd888068687d6dde465c7bba22f40f8d8a4ce25
podgorniy.inc <podgorniy.inc@gmail.com>
2023-12-14 19:07:37 +0100

    fixed untracked files

--- hair/admin.py
+++ hair/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from .models import Post
+from .models import GalleryPhoto, Page, Tag
 
-admin.site.register(Post)
+admin.site.register(Page)
+admin.site.register(Tag)
+admin.site.register(GalleryPhoto)
--- hair/models.py
+++ hair/models.py
@@ -1,21 +1,44 @@
 from django.db import models
 
 
-class Post(models.Model):
-    title = models.CharField(max_length=255, verbose_name="Название")
-    slug = models.SlugField(max_length=255, verbose_name="URL", unique=True)
-    content = models.TextField(blank=True)
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Опубликовано")
-    photo = models.ImageField(upload_to="photos/%Y/%m/%d/", blank=True)
+class Page(models.Model):
+    title = models.CharField(max_length=64)
+    slug = models.SlugField(max_length=64, verbose_name="URL", unique=True)
+    order = models.IntegerField(default=0)
 
     def __str__(self):
         return self.title
 
     class Meta:
-        verbose_name = "Статья(ю)"
-        verbose_name_plural = "Статьи"
+        ordering = ["order"]
+        verbose_name_plural = "Pages"
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=64)
+    slug = models.SlugField(max_length=64, verbose_name="URL", unique=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ["title"]
+        verbose_name = "Tag"
+        verbose_name_plural = "Tags"
+
+
+class GalleryPhoto(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.TextField(blank=True)
+    photo = models.ImageField(upload_to="photos/%Y/%m/%d")
+    tag = models.ManyToManyField(Tag, blank=True, related_name="photos")
+    created_at = models.DateTimeField(auto_now_add=True)
+    # slug = models.SlugField(max_length=64, verbose_name="URL", unique=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
         ordering = ["-created_at"]
-
-
-#    def get_absolute_url(self):
-#        return reverse("post", kwargs={"slug": self.slug})
+        verbose_name = "Gallery Image"
+        verbose_name_plural = "Gallery Images"
